fbd9247a1c63edc3488013a892caa361bf8ce48e
anusky <anusky.pitrusky@gmail.com>
2018-11-14 16:12:43 +0100

    userform - wip - cursomization
    Added password regex validation (2-4 length, one character minimum, one number minimum) 
    Added compoundComponent with children to customize error validation message
    Moved user-form from index of application to make children possible.

--- src/lib/components/user-form.js
+++ src/lib/components/user-form.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { validateType } from "../utils";
-import PropTypes from "prop-types";
-import Field from "./field";
+import React, { Component } from 'react'
+import { validateType } from '../utils'
+import PropTypes from 'prop-types'
+import Field from './field'
 
 class UserForm extends Component {
   //TODO dinamizar formulario desde fuera para que entre cualquier input sin nombre estipulado
@@ -11,6 +11,7 @@ class UserForm extends Component {
         name: PropTypes.string.isRequired,
         type: PropTypes.string.isRequired,
         mandatory: PropTypes.bool.isRequired,
+        placeholder: PropTypes.string.isRequired,
         validation: PropTypes.arrayOf(
           PropTypes.shape({
             method: PropTypes.string,
@@ -25,192 +26,198 @@ class UserForm extends Component {
     formEdit: PropTypes.object,
     id: PropTypes.string.isRequired,
     submit: PropTypes.func.isRequired
-  };
+  }
   static defaultProps = {
     formStructure: [],
-    formEdit: {},
-    id: "register",
+    id: 'register',
     submit: () => {}
-  };
+  }
   state = {
-    id: "",
-    addressList: ["City", "Postalcode", "Phone"],
+    id: '',
+    addressList: ['City', 'Postalcode', 'Phone'],
     form: {
-      firstName: "",
-      username: "",
-      email: "",
-      password1: "",
-      password2: "",
-      country: "",
-      addressPostalcode: "",
-      addressCity: "",
-      addressPhone: "",
-      addressPostalcodeInvoice: "",
-      addressCityInvoice: "",
-      addressPhoneInvoice: "",
+      firstName: '',
+      username: '',
+      email: '',
+      password1: '',
+      password2: '',
+      country: '',
+      addressPostalcode: '',
+      addressCity: '',
+      addressPhone: '',
+      addressPostalcodeInvoice: '',
+      addressCityInvoice: '',
+      addressPhoneInvoice: '',
       copyAddress: false
     },
     error: {},
     validated: {},
     mandatory: {},
     validationTypes: {}
-  };
+  }
 
   handleValidateUnitaryInput = (
     element,
     data = { value: this.state.form[element] }
   ) => {
-    let stateAux = this.state;
-    const { validationTypes } = this.state;
-    stateAux.validated[element] = false;
+    let stateAux = this.state
+    const { validationTypes } = this.state
+    stateAux.validated[element] = false
     validationTypes[element].length > 0
       ? validationTypes[element].forEach(actualType => {
-          const { method, withwho } = actualType;
+          const { method, withwho } = actualType
           actualType.withValue =
-            method === "comparison" ? this.state.form[withwho] : false;
+            method === 'comparison' ? this.state.form[withwho] : ''
           stateAux.validated[element] =
-            stateAux.validated[element] || validateType(actualType, data);
+            stateAux.validated[element] || validateType(actualType, data)
         })
-      : (stateAux.validated[element] = true);
-    return stateAux;
-  };
+      : (stateAux.validated[element] = true)
+    return stateAux
+  }
 
   handleValidateInputs = () => {
-    let stateAux = this.state;
+    let stateAux = this.state
     if (this.props.formEdit) {
-      stateAux.form = this.props.formEdit;
+      stateAux.form = this.props.formEdit
       for (let el in stateAux.form) {
         stateAux = this.handleValidateUnitaryInput(el, {
           value: stateAux.form[el]
-        });
+        })
       }
     }
-    this.setState(stateAux);
-  };
+    this.setState(stateAux)
+  }
   static getDerivedStateFromProps(props, state) {
     //we only setup state the first time depending on props data
     if (props.id !== state.id) {
-      const { formStructure = [] } = props;
+      const { formStructure = [] } = props
       let stateAux = {
         mandatory: {},
         error: {},
         validated: {},
-        validationTypes: {}
-      };
+        validationTypes: {},
+        form: {}
+      }
       formStructure.forEach(el => {
-        const { mandatory, name, validation } = el;
-        stateAux.mandatory[name] = mandatory;
-        stateAux.error[name] = false;
-        stateAux.validated[name] = true;
-        stateAux.validationTypes[name] = validation;
-      });
+        const { mandatory, name, validation } = el
+        stateAux.mandatory[name] = mandatory
+        stateAux.error[name] = false
+        stateAux.validated[name] = true
+        stateAux.validationTypes[name] = validation
+        stateAux.form[name] = ''
+      })
       return {
         mandatory: stateAux.mandatory,
         error: stateAux.error,
         validated: stateAux.validated,
         validationTypes: stateAux.validationTypes,
+        form: stateAux.form,
         id: props.id
-      };
+      }
     }
     // No state update necessary
-    return null;
+    return null
   }
   componentDidMount(prevProps, prevState) {
-    this.handleValidateInputs();
+    this.handleValidateInputs()
   }
 
   handleGenericChange = ({ value, name }) => {
-    let formAux = this.state.form;
-    if (name.indexOf("address") !== -1 && this.state.form.copyAddress) {
-      formAux = { ...formAux, [`${name}Invoice`]: value };
+    let formAux = this.state.form
+    if (name.indexOf('address') !== -1 && this.state.form.copyAddress) {
+      formAux = { ...formAux, [`${name}Invoice`]: value }
     }
-    formAux = { ...formAux, [name]: value };
+    formAux = { ...formAux, [name]: value }
     const formError = { ...this.state.error, [name]: false },
-      formValidated = { ...this.state.validated, [name]: true };
+      formValidated = { ...this.state.validated, [name]: true }
     this.setState({
       form: formAux,
       error: formError,
       validated: formValidated
-    });
-  };
+    })
+  }
 
   handleChange = (event, { name, type, value } = event.target) => {
     switch (type) {
-      case "checkbox":
-        this.handleCopyAddressChange(event);
-        break;
+      case 'checkbox':
+        this.handleCopyAddressChange(event)
+        break
       default:
-        this.handleGenericChange({ value, name });
+        this.handleGenericChange({ value, name })
     }
-  };
+  }
 
   handleValidate = (event, { name } = event.target) => {
-    console.log("validating name ", name);
+    console.log('validating name ', name)
     const { [name]: validated } = this.handleValidateUnitaryInput(
       name
-    ).validated;
+    ).validated
     this.setState({
       validated: {
         ...this.state.validated,
         [event.target.id]: validated
       }
-    });
-  };
+    })
+  }
 
   handleSubmit = event => {
-    event.preventDefault();
+    event.preventDefault()
     let stateErrorAux = this.state.error,
-      sendData = true;
+      sendData = true
     for (let el in this.state.form) {
-      this.handleValidateUnitaryInput(el);
+      this.handleValidateUnitaryInput(el)
       stateErrorAux[el] =
         !this.state.validated[el] ||
         !validateType(
-          { method: "mandatory" },
+          { method: 'mandatory' },
           {
             value: this.state.form[el],
             mandatory: this.state.mandatory[el]
           }
-        );
-      sendData = sendData && !stateErrorAux[el];
+        )
+      sendData = sendData && !stateErrorAux[el]
     }
     this.setState({ error: stateErrorAux }, () => {
       if (sendData) {
-        this.props.submit(this.state.form);
+        this.props.submit(this.state.form)
       }
-    });
-  };
+    })
+  }
 
   //copyAddress
   handleCopyAddressChange = event => {
-    let stateAux = this.state;
-    stateAux.form.copyAddress = event.target.checked;
+    let stateAux = this.state
+    stateAux.form.copyAddress = event.target.checked
     stateAux.addressList.forEach(type => {
       stateAux.form[`address${type}Invoice`] = event.target.checked
         ? stateAux.form[`address${type}`]
-        : "";
+        : ''
       stateAux.validated[`address${type}Invoice`] = event.target.checked
         ? stateAux.validated[`address${type}`]
-        : "";
-    });
-    this.setState(stateAux);
-  };
+        : ''
+    })
+    this.setState(stateAux)
+  }
 
   onClickPrueba = event => {
-    console.log("caca");
-  };
+    console.log('caca')
+  }
+
+  static Error = props => {
+    return props.children
+  }
 
   render() {
     const validatedStyle = valid => ({
-      color: valid ? "green" : "red",
-      display: valid ? "none" : "block"
-    });
-    const { formStructure = [] } = this.props;
+      color: valid ? 'green' : 'red',
+      display: valid ? 'none' : 'block'
+    })
+    const { formStructure = [] } = this.props
     return (
       <form className="c-userform" onSubmit={this.handleSubmit}>
         <h3> automatized </h3>
         {formStructure.map(el => {
-          const { name, type, options } = el;
+          const { name, type, options, placeholder } = el
           return (
             <Field
               key={name}
@@ -221,13 +228,18 @@ class UserForm extends Component {
               type={type}
               name={name}
               options={options}
+              placeholder={placeholder}
               validation={
                 <div style={validatedStyle(this.state.validated[name])}>
                   {`validated: ${this.state.validated[name]}`}
                 </div>
               }
-            />
-          );
+            >
+              <Field.Error validated={this.state.validated[name]}>
+                {this.props.children}
+              </Field.Error>
+            </Field>
+          )
         })}
         <button type="submit" variant="contained" className={`buttona`}>
           Submit
@@ -236,7 +248,7 @@ class UserForm extends Component {
           SubmitPrueba
         </button>
       </form>
-    );
+    )
   }
 }
-export default UserForm;
+export default UserForm
